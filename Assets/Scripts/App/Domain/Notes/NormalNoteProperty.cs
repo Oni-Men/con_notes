3160fd4c1ccfb3209fcf82d0f2077f6cf214f7bc
Oni-Men <sensyaheis@gmail.com>
2023-03-20 18:25:34 +0900

    remove: unused files

--- Assets/Scripts/App/Domain/Notes/NormalNoteProperty.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace App.Domain.Notes
-{
-    public class NormalNoteProperty : NoteProperty
-    {
-        public NormalNoteProperty(float begin) : base(begin, NoteType.Single)
-        {
-        }
-    }
-}
